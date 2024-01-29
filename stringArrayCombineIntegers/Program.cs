5e43894c9bca8a62bdde82dd8cde81c5840cc1ce
EddEnt <61289536+EddEnt@users.noreply.github.com>
2024-01-29 09:05:21 -0500

    Initial commit

---
+++ stringArrayCombineIntegers/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] values = new string[] { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0;
+string message = "";
+
+foreach (var value in values)
+{
+    decimal outNumber;
+
+    if (decimal.TryParse(value, out outNumber))
+    {
+        total += outNumber;
+    }
+    else
+    {
+        message += value;
+    }
+}
+
+Console.WriteLine($"String Message: {message}");
+Console.WriteLine($"Decimal Total: {total}");
